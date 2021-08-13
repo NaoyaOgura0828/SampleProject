f0c9dc015566f27c981bb9e46f272cde74596429
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-08-13 20:53:15 +0900

    sayhelloメソッドの追加

--- src/jp/learningdesign/javastudy/Staffinfo.java
+++ src/jp/learningdesign/javastudy/Staffinfo.java
@@ -4,14 +4,20 @@ class Staff {
     String name; // 名前
     int staffid; // 社員番号
     String email; // メールアドレス
+
+    public void sayhello() {
+        System.out.println("Hello " + this.name); // Helloの後に名前を付ける(Staffからnameを呼び出す)
+    }
 }
 
 public class Staffinfo {
     public static void main(String[] args) {
         Staff yamada = new Staff(); // Staffクラスのyamadaオブジェクトを作成
-        yamada.name = "Taro Yamada"; // yamadaオブジェクトのnameにTaro Yamadaと代入
+        yamada.name = "Taro Yamada"; // yamadaオブジェクトのnameに"Taro Yamada"を格納
 
-        System.out.println(yamada.name); // yamadaオブジェクトのnameを表示する
+        yamada.sayhello(); // sayhelloメソッドを呼び出す処理
+
+        // System.out.println(yamada.name); // yamadaオブジェクトのnameを表示する
 
     }
 }
