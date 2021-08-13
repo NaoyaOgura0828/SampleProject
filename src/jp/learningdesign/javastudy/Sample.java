765a99a1f741f8cc7367ec263f660c5a2c0f3110
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-08-13 11:49:41 +0900

    Sample.javaを記述

--- src/jp/learningdesign/javastudy/Sample.java
+++ src/jp/learningdesign/javastudy/Sample.java
@@ -1,4 +1,15 @@
 package jp.learningdesign.javastudy;
 
 public class Sample {
+    /* Sampleは、"Hello Eclipse!"と表示するプログラムです。 */
+
+    /**
+     * Javadoc形式
+     * author: Naoya Ogura
+     * copyright:
+     */
+    public static void main(String[] args) {
+        // 次の行で、コンソールに文字列を出力します。
+        System.out.println("こんにちは、エクリプス!");
+    }
 }
