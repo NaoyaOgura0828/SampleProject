d2f3920dc0de9f0dec5ab911e9bcbb8fa7edd7b3
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-08-13 20:43:56 +0900

    コメントを追記

--- src/jp/learningdesign/javastudy/Staffinfo.java
+++ src/jp/learningdesign/javastudy/Staffinfo.java
@@ -1,13 +1,17 @@
 package jp.learningdesign.javastudy;
 
 class Staff {
-    String name;
-    int staffid;
-    String email;
+    String name; // 名前
+    int staffid; // 社員番号
+    String email; // メールアドレス
 }
 
 public class Staffinfo {
     public static void main(String[] args) {
+        Staff yamada = new Staff();
+        yamada.name = "Taro Yamada";
+
+        System.out.println(yamada.name);
 
     }
 }
