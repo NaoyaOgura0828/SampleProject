504f40690a57dc80b9f14480433da59df00a04aa
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-08-13 22:05:21 +0900

    加減乗除の計算

--- src/jp/learningdesign/javastudy/calc.java
+++ src/jp/learningdesign/javastudy/calc.java
@@ -3,6 +3,14 @@ package jp.learningdesign.javastudy;
 public class calc {
 
     public static void main(String[] args) {
-
+        // 算術演算子 + - * / %
+        int a = 15;
+        int b = 4;
+        System.out.println(a + b); // 足し算の出力
+        // System.out.println("\n"); // 改行の挿入
+        System.out.println(a - b); // 引き算の出力
+        System.out.println(a * b); // 掛け算の出力
+        System.out.println(a / b); // 割り算の出力
+        System.out.println(a % b); // 余りの出力
     }
 }
