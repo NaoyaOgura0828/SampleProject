a932cacd65706de6c778f8a43313424e953ee097
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-08-13 11:51:57 +0900

    Staffinfo.javaの記述

---
+++ src/jp/learningdesign/javastudy/Staffinfo.java
@@ -0,0 +1,13 @@
+package jp.learningdesign.javastudy;
+
+class Staff {
+    String name;
+    int staffid;
+    String email;
+}
+
+public class Staffinfo {
+    public static void main(String[] args) {
+
+    }
+}
