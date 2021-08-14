bf334d556a003395190cf41934bd5c3d8664da6f
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-08-14 20:14:25 +0900

    BMIを計算し表示する

--- src/jp/learningdesign/javastudy/BmiCalc.java
+++ src/jp/learningdesign/javastudy/BmiCalc.java
@@ -6,6 +6,12 @@ public class BmiCalc {
     *  体重 kg、 身長 m
     */
     public static void main(String[] args) {
+        double weight, height, bmi;
 
+        weight = 71; // 体重を入力
+        height = 1.8; // 身長を入力
+        bmi = weight / (height * height); // BMIを計算
+
+        System.out.println(bmi); // BMIを出力
     }
 }
