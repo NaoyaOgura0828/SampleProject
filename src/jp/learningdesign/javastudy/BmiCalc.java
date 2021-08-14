ec30d743d459725c0bd66d54e3690d9a1cf776cc
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-08-14 19:57:57 +0900

    BmiCalc.javaの追加

---
+++ src/jp/learningdesign/javastudy/BmiCalc.java
@@ -0,0 +1,11 @@
+package jp.learningdesign.javastudy;
+
+public class BmiCalc {
+    /* BMI（体格指数）を計算するプログラム
+    *  BMI = 体重 / (身長 * 身長)
+    *  体重 kg、 身長 m
+    */
+    public static void main(String[] args) {
+
+    }
+}
