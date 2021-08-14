a892ed10c58703338649c73771313e7d57938bb0
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-08-14 20:42:17 +0900

    CountUp.javaの完成

--- src/jp/learningdesign/javastudy/CountUp.java
+++ src/jp/learningdesign/javastudy/CountUp.java
@@ -1,7 +1,10 @@
 package jp.learningdesign.javastudy;
 
 public class CountUp {
+    /* 0から10未満の数字を1ずつカウントアップしていくプログラム */
     public static void main(String[] args) {
-
+        for (int i = 0;i < 10; i++) { // 0から10未満の数字を1ずつforループでカウントアップしていきiに代入する
+            System.out.println(i); // iを出力する
+        }
     }
 }
